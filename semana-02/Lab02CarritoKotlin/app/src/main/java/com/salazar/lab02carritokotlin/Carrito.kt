85b284d975e134fb3dcf0608159c6d39dc23da19
Manuel <asfgdsasxc@gmail.com>
2026-08-28 00:59:18 -0500

    Agrega data class Producto y funciones de subtotal, IGV y total

--- semana-02/Lab02CarritoKotlin/app/src/main/java/com/salazar/lab02carritokotlin/Carrito.kt
+++ semana-02/Lab02CarritoKotlin/app/src/main/java/com/salazar/lab02carritokotlin/Carrito.kt
@@ -1,5 +1,25 @@
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    var cantidad: Int
+)
+
 fun main() {
     println("=========================================")
     println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
     println("=========================================")
+
+    val nombreCliente = "Juan Leon"
+    val carrito = mutableListOf<Producto>()
+    println("Cliente: $nombreCliente")
+    println()
+
+    carrito.add(Producto("Laptop HP", 2500.0, 1))
+    carrito.add(Producto("Mouse Logitech", 45.5, 2))
+    carrito.add(Producto("Teclado Mecánico", 120.0, 1))
+    carrito.add(Producto("Monitor Samsung", 750.0, 1))
+
+    for (producto in carrito) {
+        println("Producto agregado: ${producto.nombre}")
+    }
 }
