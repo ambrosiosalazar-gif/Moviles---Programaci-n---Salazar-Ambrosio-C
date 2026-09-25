92093c1d39bc1c2fdd9d139baac7ec52c70ce679
Salazar Ambrosio Gerardo <ambrosio.salazar@tecsup.edu.pe>
2026-09-25 13:19:02 -0500

    Fija la paleta morada de marca en el tema y desactiva dynamicColor para mantener consistencia visual con el diseño de Clinica Salud+

--- semana-05/app/src/main/java/com/salazar/clinicasaludmas/ui/theme/Color.kt
+++ semana-05/app/src/main/java/com/salazar/clinicasaludmas/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.salazar.clinicasaludmas.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Paleta morada de Clínica Salud+
+val PurplePrimary = Color(0xFF6750A4)
+val PurpleContainer = Color(0xFFEADDFF)
+val PurpleSecondary = Color(0xFF625B71)
+val Background = Color(0xFFFFFBFE)
+val Surface = Color(0xFFFFFBFE)
